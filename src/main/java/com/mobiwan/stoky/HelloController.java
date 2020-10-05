5dcbe821cd6e57693a1f004dea2097e38be587f2
Jorge Costa <jfcostta@gmail.com>
2020-10-05 19:57:20 -0300

    very first commit

---
+++ src/main/java/com/mobiwan/stoky/HelloController.java
@@ -0,0 +1,24 @@
+package com.mobiwan.stoky;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(
+		value = { "/", "/index" }, 
+		method = RequestMethod.GET)
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@RequestMapping(value = "*", method = RequestMethod.GET)
+	@ResponseBody
+	public String getFallback() {
+    	return "Endpoint not defined: fallback for GET Requests";
+	}
+	
+}
---
+++ src/test/java/com/mobiwan/stoky/StokyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiwan.stoky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
