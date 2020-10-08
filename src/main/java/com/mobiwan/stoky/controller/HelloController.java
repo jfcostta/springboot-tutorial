e2a11fb326bdd89533437ce2ffcfc0db190f846a
Jorge Costa <jfcostta@gmail.com>
2020-10-08 12:45:01 -0300

    added Hello domain and controller (returns JSON)

--- src/main/java/com/mobiwan/stoky/controller/HelloController.java
+++ src/main/java/com/mobiwan/stoky/controller/HelloController.java
@@ -1,24 +1,20 @@
 package com.mobiwan.stoky.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.mobiwan.stoky.domain.Hello;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
-	/** @ref https://www.baeldung.com/spring-requestmapping */
-	@RequestMapping(
-		value = { "/", "/index" }, 
-		method = RequestMethod.GET)
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    /** @ref https://spring.io/guides/gs/rest-service/ */
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping(value = "*", method = RequestMethod.GET)
-	@ResponseBody
-	public String getFallback() {
-    	return "Endpoint not defined: fallback for GET Requests";
+	@GetMapping("/hello")
+	public Hello greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Hello(counter.incrementAndGet(), String.format(template, name));
 	}
-	
 }
---
+++ src/main/java/com/mobiwan/stoky/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.mobiwan.stoky.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class IndexController {
+	/** @ref https://www.baeldung.com/spring-requestmapping */
+	@RequestMapping(
+		value = { "/", "/index" }, 
+		method = RequestMethod.GET)
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@RequestMapping(value = "*", method = RequestMethod.GET)
+	@ResponseBody
+	public String getFallback() {
+    	return "Endpoint not defined: fallback for GET Requests";
+	}
+	
+}
